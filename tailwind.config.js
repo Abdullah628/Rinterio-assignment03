06642e746babd6f34d55ca03e2869102117ce46c
abdullah628 <abdullah628360@gmail.com>
2024-08-22 12:35:03 +0600

    navbar design complete

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           clifford: '#da373d',
+//           'primary-color': 'rgba(171, 239, 95, 1)',
+//         }
+//       }
+//     }
+//   }
